346ca34607bc233b7bd5606e411c5c0019305c51
VanshDhar <37364319+VanshDhar@users.noreply.github.com>
2021-08-09 16:55:06 -0700

    Update model_builder_temp.py

--- pysot/models/model_builder_temp.py
+++ pysot/models/model_builder_temp.py
@@ -136,3 +136,5 @@ class ModelBuilder(nn.Module):
             mask_loss = None
             outputs['total_loss'] += cfg.TRAIN.MASK_WEIGHT * mask_loss
             outputs['mask_loss'] = mask_loss
+           
+        return outputs
